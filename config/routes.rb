2431958e3cd9f5dd90bcc685c27f69eff1d7b62a
masa335477 <fujisakim01@gmail.com>
2026-03-06 10:49:30 +0900

    add:掲示板作成画面の表示まで実装

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
 
-  resources :boards, only: %i[index]
+  resources :users, only: %i[new create]
+  resources :boards, only: %i[index new]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
