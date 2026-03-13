b5e60f42b5b41b95ee38b1b756decbbc9eee9e47
masa335477 <fujisakim01@gmail.com>
2026-03-13 14:43:19 +0900

    掲示板の編集と削除が正常に実装されました

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -23,6 +23,26 @@ class BoardsController < ApplicationController
       @comments = @board.comments.includes(:user).order(created_at: :desc)
     end
 
+    def edit
+      @board = current_user.boards.find(params[:id])#すべてのユーザーがアクセスできると、他のユーザーの投稿を編集できてしまうため、current_user.boardsから探すようにする
+    end
+
+    def update
+      @board = current_user.boards.find(params[:id])
+      if @board.update(board_params)
+        redirect_to @board, success: t('defaults.flash_message.updated', item: Board.model_name.human)
+      else
+        flash.now[:danger] = t('defaults.flash_message.not_updated', item: Board.model_name.human)
+        render :edit, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      @board = current_user.boards.find(params[:id])
+      @board.destroy!
+      redirect_to boards_path, success: t('defaults.flash_message.deleted', item: Board.model_name.human), status: :see_other
+    end
+
     private
 
     def board_params
